3854dee1ca130a8ddd0f29dd4b5287feb57791db
crndolo <ndoloraphael@gmail.com>
2020-07-14 07:50:05 +0300

    Creating page to land after homepage.

--- resources/views/nyumbani/karibu.blade.php
+++ resources/views/nyumbani/karibu.blade.php
@@ -1,28 +1,121 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta name="viewport" content="width=device-width initial-scale=1.0" charset="UTF-8">
-	<title>Les Magnifique</title>
-	
-
-	<link rel="stylesheet" href="assets/font-awesome/css/all.css">
-	<link rel="stylesheet" href="assets/font-awesome/css/all.min.css">
-	
-
-	<link rel="stylesheet" href="assets/css/styles.css" type="text/css">
-</head>
-<body>
-	<!-- BEGIN HEADER -->
-	<header class="header">
+	<head>
+		<meta name="viewport" content="width=device-width initial-scale=1.0" charset="UTF-8">
+		<title>Muungano Theatre</title>
 		
-		Karibu section
 
-	</header>
-	<!-- END HEADER -->
+		<link rel="stylesheet" href="assets/font-awesome/css/all.css">
+		<link rel="stylesheet" href="assets/font-awesome/css/all.min.css">
+		
 
-	<!-- BEGIN JAVASCRIPT FILES -->
-	<script src="{{ URL::asset('assets/js/jquery.min.js') }}" type="text/javascript"></script>
-	<script  src="{{ URL::asset('assets/js/jquery.slideshow.min.js') }}" type="text/javascript"></script>
-	<!-- END JAVASCRIPT FILES -->
-</body>
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+		<link rel="stylesheet" href="assets/css/other_styles.css" type="text/css">
+	</head>
+	<body>
+		
+		<!-- BEGIN LOGO
+			<div class="container-fluid">
+				<div class="row ">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 logo-yetu">
+						<a href="#"> Muungano Threatre</a>
+					</div>
+				</div>
+			</div>
+		<!-- END LOGO 
+
+		<!-- BEGIN NAVBAR 
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsemenu" aria-expanded="false">
+						<span class="sr-only">Toggle Navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<div class="collapse navbar-collapse" id="collapsemenu">
+					<ul class="nav navbar-nav">
+						<li><a href=""> Movies </a></li>
+						<li><a href=""> Series </a></li>
+						<li><a href=""> Animation </a></li>
+					</ul>
+
+					<form class="navbar-form navbar-left" role="search"> 
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Search...">
+							<button type="submit" class="btn btn-default"> Search </button>
+						</div>
+					</form>
+				</div>
+
+			</div>
+		</nav>
+		<!-- END NAVBAR 
+
+		<!-- BEGIN CONTAINER 
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#" > <img src="assets/img/filamu/1.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"> <img src="assets/img/filamu/2.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"> <img src="assets/img/filamu/3.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"> <img src="assets/img/filamu/4.jpg" alt="Logo" > </a>		
+				</div>
+			
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"  > <img src="assets/img/filamu/5.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"  > <img src="assets/img/filamu/6.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"  > <img src="assets/img/filamu/7.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"  > <img src="assets/img/filamu/8.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#" > <img src="assets/img/filamu/1.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"> <img src="assets/img/filamu/2.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"> <img src="assets/img/filamu/3.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#"> <img src="assets/img/filamu/4.jpg" alt="Logo" > </a>		
+				</div>
+			
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#" class="thumbnail" > <img src="assets/img/filamu/5.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#" class="thumbnail" > <img src="assets/img/filamu/6.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#" class="thumbnail" > <img src="assets/img/filamu/7.jpg" alt="Logo" > </a>		
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-2 col-lg-2 filamu">
+					<a href="#" class="thumbnail" > <img src="assets/img/filamu/8.jpg" alt="Logo" > </a>		
+				</div>
+			</div>
+		</div>
+		<!-- END CONTAINER 
+
+		<!-- BEGIN JAVASCRIPT LINKS 
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/bootstrap/js/bootstrap.js"></script>
+		<!-- END JAVASCRIPT LINKS -->
+	</body>
 </html>
--- resources/views/nyumbani/mwanzo.blade.php
+++ resources/views/nyumbani/mwanzo.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta name="viewport" content="width=device-width initial-scale=1.0" charset="UTF-8">
-	<title>Les Magnifique</title>
+	<title>Muungano Theatres</title>
 	
 
 	<link rel="stylesheet" href="assets/font-awesome/css/all.css">
