bb14cb14f5c63916cb20ec84cd2abb6f6be168ec
crndolo <ndoloraphael@gmail.com>
2020-07-14 10:58:16 +0300

    Changing configurations to ensure database migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
+            $table->rememberToken();
+            $table->string('password');
             $table->string('last_name');
+            $table->string('first_name');
             $table->string('email')->unique();
             $table->integer('telephone')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
